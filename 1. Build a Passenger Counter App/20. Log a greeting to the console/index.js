7d877875c4f1e6a578f24da05cfe6c172f3209ea
mathieu hollande <mathieuhollande@hotmail.fr>
2022-01-26 15:18:16 +0100

    Basic muscle memory

--- 1. Build a Passenger Counter App/20. Log a greeting to the console/index.js
+++ 1. Build a Passenger Counter App/20. Log a greeting to the console/index.js
@@ -3,5 +3,8 @@
 
 // Create a third variable, myGreeting, that contatenates the two strings
 // Log myGreeting to the console
+let name = " Math"
+let greeting = "Hi, my name is"
 
-
+let myGreeting = greeting + name
+console.log(myGreeting)
