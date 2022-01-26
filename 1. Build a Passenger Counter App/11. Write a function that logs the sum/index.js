6563a7adba6e91fca7c9e90cd11a3cee268053c5
mathieu hollande <mathieuhollande@hotmail.fr>
2022-01-26 13:32:37 +0100

    increment function

--- 1. Build a Passenger Counter App/11. Write a function that logs the sum/index.js
+++ 1. Build a Passenger Counter App/11. Write a function that logs the sum/index.js
@@ -4,3 +4,18 @@ let lap3 = 36
 
 // Create a function that logs out the sum of all the lap times
 
+function totalLap(){
+  console.log(lap1+lap2+lap3)
+}
+
+totalLap()
+
+
+// Another way to do it
+
+// function totalLap(){
+//   let increment = lap1 + lap2 + lap3
+//   console.log(increment)
+// }
+
+// totalLap()
