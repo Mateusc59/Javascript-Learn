3dfc8c00ea01d89b4fee9ac08a9432a01f4031a8
mathieu hollande <mathieuhollande@hotmail.fr>
2022-01-26 14:50:50 +0100

    make incrementation display

--- 1. Build a Passenger Counter App/16. Display the count with innerText/index.js
+++ 1. Build a Passenger Counter App/16. Display the count with innerText/index.js
@@ -1,9 +1,11 @@
 // grab the count-el element, store it in a countEl variable
+let countEL = document.getElementsById('count-el')
+
 let count = 0
+
 
 function increment() {
     count = count + 1
+    countEl.innerText = count
     // set countEl's innerText to the count
 }
-
-
