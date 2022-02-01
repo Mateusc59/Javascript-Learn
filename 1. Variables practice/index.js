1218dded81899deba478efd53c19da60f30a2866
mathieu hollande <mathieuhollande@hotmail.fr>
2022-02-01 15:46:13 +0100

    add Black jack learning game JS

--- 1. Variables practice/index.js
+++
@@ -1,7 +0,0 @@
-// Create two variables, firstName and lastName
-let firstName = "Mathieu"
-let lastName = "Hollande"
-// Concatenate the two variables into a third variable called fullName
-let fullName = firstName + " " + lastName
-// Log fullName to the console
-console.log(fullName)
