6a63f61d49416f559625e86dbf6956f3b548d539
mathieu hollande <mathieuhollande@hotmail.fr>
2022-02-07 13:06:45 +0100

    Call a random number with function

--- III. Build a Blackjack game/40. Completing our dice function/index.js
+++ III. Build a Blackjack game/40. Completing our dice function/index.js
@@ -1,5 +1,13 @@
 // Try to modify the expression so that we get a range from 1 to 6
-let randomNumber = Math.floor( Math.random() * 6 )
 
-console.log(randomNumber)
 
+let randomNumber = Math.floor(Math.random() * 6) + 1
+
+// Create a function, rollDice(), that returns a random number between 1 and 6
+
+function rollDice() {
+  randomNumber = Math.floor(Math.random() * 6) + 1
+  return randomNumber
+}
+
+console.log(rollDice())
