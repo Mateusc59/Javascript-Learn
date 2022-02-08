2647ef28cf01d2608c3bfe60402951ae2464a00e
mathieu hollande <mathieuhollande@hotmail.fr>
2022-02-08 15:55:59 +0100

    practice loop

--- IIII practice time/4. Practice time - part 2/6. Rock papers scissors/index.js
+++ IIII practice time/4. Practice time - part 2/6. Rock papers scissors/index.js
@@ -2,4 +2,25 @@ let hands = ["rock", "paper", "scissor"]
 
 // Create a function that returns a random item from the array
 
+// work but not the right answer:
+//function battle() {
+ // hands = (Math.floor(Math.random() * 3) + 1)
+ // if (hands === 1) {
+  //  return "rock"
+ // } else if (hands === 2) {
+   // return "paper"
+ // } else
+   // return "scissor"
+  //for (let i = 0; i < hands.length; i++){
+  //}
+//}
 
+//console.log(battle())
+
+
+function getHand() {
+  let randomIndex = Math.floor(Math.random() * 3)
+  return hands[randomIndex]
+}
+
+console.log(getHand())
