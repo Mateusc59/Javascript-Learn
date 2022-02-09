b2602bb0ae6c1fd0fd98987e0651bfae63be1de4
mathieu hollande <mathieuhollande@hotmail.fr>
2022-02-09 15:32:37 +0100

    add with innerHtml

--- V. Build chrome extensions/5. Build a Chrome Extension/11. Push the value from the input field/index.js
+++ V. Build chrome extensions/5. Build a Chrome Extension/11. Push the value from the input field/index.js
@@ -3,11 +3,11 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 
 inputBtn.addEventListener("click", function() {
-    // Push the value from the inputEl into the myLeads array 
+    // Push the value from the inputEl into the myLeads array
     // instead of the hard-coded "www.awesomeleads.com" value
+  myLeads.push(inputEl.value)
+
     // Google -> "get value from input field javascript"
     myLeads.push("www.awesomelead.com")
     console.log(myLeads)
 })
-
-
