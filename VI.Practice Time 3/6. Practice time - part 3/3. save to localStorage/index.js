32fe4ffd2dd5f9cbb090ff8e0c09889281a33d67
mathieu hollande <mathieuhollande@hotmail.fr>
2022-02-15 10:37:50 +0100

    practice time local storage

--- VI.Practice Time 3/6. Practice time - part 3/3. save to localStorage/index.js
+++ VI.Practice Time 3/6. Practice time - part 3/3. save to localStorage/index.js
@@ -1,3 +1,14 @@
 // Save a value to localStorage
 // Delete your code and refresh the page
-// Fetch your value from localStorage and log it out
+// Fetch your value from localStorage and log it outmyArray = ["row","column","tabs"]
+myArray = ["row", "column", "tabs"]
+
+localStorage.setItem("myArray", JSON.stringify(myArray))
+console.log(localStorage.getItem("myArray")
+)
+
+//also
+
+let array = localStorage.getItem("myArray")
+console.log(array)
+// must fetch the data and log out the result
